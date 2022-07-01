e0b47b4cc6e6a333ccd712ee98f0483d64d9b360
Asmaalily <semo.nasser@gmail.com>
2022-07-01 06:20:35 +0200

    make it better

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+app.use(express.static(__dirname + '/dist/QueenBee'))
+
+app.get ('*',(req,res)=>{
+  res.sendFile('index.html',{root:'dist/QueenBee/'})
+})
+
+app.listen(process.env.PORT)
