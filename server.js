dcb2597df806cd8c0c849a13413f1d7edf23d118
Asmaalily <semo.nasser@gmail.com>
2022-07-01 22:07:19 +0200

    make it better

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path');
 
 const app = express()
-app.use(express.static(__dirname + './dist/QueenBee'))
+app.use(express.static(__dirname + './dist/QueenBee/browser'))
 
 app.get ('/*',(req,res)=>{
   res.sendFile('index.html',{root:'dist/QueenBee/'})
