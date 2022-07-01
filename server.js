52efa042097efab3fb74b69720afae3465ef4488
Asmaalily <semo.nasser@gmail.com>
2022-07-02 00:29:48 +0200

    make it better

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const path = require('path');
-const compression = require('compression')
 
 const app = express()
 app.use(express.static(__dirname + '/dist/QueenBee/browser'))
-app.use(compression())
 
 
 app.get ('*',(req,res)=>{
