e6e3e87b9dad8e9ebdb934c7d88a5065c2e51317
Asmaalily <semo.nasser@gmail.com>
2022-07-01 22:11:04 +0200

    make it better

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express')
 const path = require('path');
 
 const app = express()
-app.use(express.static(__dirname + './dist/QueenBee/browser'))
+app.use(express.static(__dirname + '/dist/QueenBee'))
 
-app.get ('/*',(req,res)=>{
+app.get ('*',(req,res)=>{
   res.sendFile('index.html',{root:'dist/QueenBee/'})
 })
 
-app.listen(process.env.PORT || 8080)
+app.listen(process.env.PORT )
