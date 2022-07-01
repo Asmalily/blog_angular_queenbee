d9bf87207b497938ca0f3826944c2799a44204f0
Asmaalily <semo.nasser@gmail.com>
2022-07-01 21:59:08 +0200

    make it better

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const app = express()
-app.use(express.static(__dirname + '/dist/QueenBee'))
 const path = require('path');
 
-app.get ('*',(req,res)=>{
+const app = express()
+app.use(express.static(__dirname + './dist/QueenBee'))
+
+app.get ('/*',(req,res)=>{
   res.sendFile('index.html',{root:'dist/QueenBee/'})
 })
 
